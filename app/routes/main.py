7beeae8921334ff4bcff298fe8e90a5365f14bc2
Curtis Hamm <curt.hamm@gmail.com>
2025-09-01 15:55:48 -0500

    pages and routes created

--- app/routes/main.py
+++ app/routes/main.py
@@ -6,6 +6,14 @@ main_blueprint = Blueprint('main', __name__)
 def home():
     return render_template('index.html')
 
+@main_blueprint.route('/lies')
+def lies():
+    return render_template('index.html')
+
+@main_blueprint.route('/deception')
+def deception():
+    return render_template('deception.html')
+
 @main_blueprint.route('/about')
 def about():
     return render_template('about.html')
